f73684277c370fe17321c296fc0422e189693b0b
marwin1991 <peter.zmilczak@gmail.com>
2019-06-22 23:57:55 +0200

    navbar

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import MainPage from "./MainPage";
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee, faUser, faBell, faCog, faSignOutAlt} from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee, faUser, faBell, faCog, faSignOutAlt);
+
 
 const App = () => {
     return (
--- src/app/MainPage.js
+++ src/app/MainPage.js
@@ -1,4 +1,12 @@
 import React from "react";
+import Route from "react-router-dom/es/Route";
+import Navbar from "react-bootstrap/es/Navbar";
+import Nav from "react-bootstrap/es/Nav";
+import NavDropdown from "react-bootstrap/es/NavDropdown";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome/index.es";
+import {faSignOutAlt} from "@fortawesome/free-solid-svg-icons/index";
+import Button from "react-bootstrap/es/Button";
+
 
 class MainPage extends React.Component {
     state = {
@@ -8,6 +16,34 @@ class MainPage extends React.Component {
     render() {
         return (
             <div>
+                <Navbar collapseOnSelect expand="lg" bg="success" variant="dark">
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" fill>
+                        <Nav>
+                            <Nav.Link href="#features"><FontAwesomeIcon icon="user" size="lg" /></Nav.Link>
+                            <Nav.Link href="#pricing"><FontAwesomeIcon icon="bell" size="lg" /></Nav.Link>
+                        </Nav>
+                        <Nav className="mx-auto" variant="pills" defaultActiveKey="/home">
+                            <Nav.Item>
+                                <Button variant="success" style={{minWidth:120}}>Borrow</Button>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Button variant="danger" style={{minWidth:120}}>Lend</Button>
+                            </Nav.Item>
+                        </Nav>
+                        <Nav >
+                            <NavDropdown title={<FontAwesomeIcon icon="cog" size="lg" />} id="collasible-nav-dropdown" drop="left" >
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#pricing"><FontAwesomeIcon icon="sign-out-alt" size="lg" /></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+
                 {this.state.applicationTile}
             </div>
         );

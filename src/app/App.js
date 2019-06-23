e4f5dd1d2fda139b104b780c7e1f31f9ec3cb46f
marwin1991 <peter.zmilczak@gmail.com>
2019-06-23 20:03:44 +0200

    item look

--- src/app/App.js
+++ src/app/App.js
@@ -3,9 +3,9 @@ import MainPage from "./MainPage";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee, faUser, faBell, faCog, faSignOutAlt, faChevronRight, faChevronLeft} from '@fortawesome/free-solid-svg-icons'
+import { faCheckSquare, faCoffee, faUser, faBell, faCog, faSignOutAlt, faChevronRight, faChevronLeft, faCheckCircle, faExclamationCircle, faClock} from '@fortawesome/free-solid-svg-icons'
 
-library.add(fab, faCheckSquare, faCoffee, faUser, faBell, faCog, faSignOutAlt, faChevronRight, faChevronLeft);
+library.add(fab, faCheckSquare, faCoffee, faUser, faBell, faCog, faSignOutAlt, faChevronRight, faChevronLeft, faCheckCircle, faExclamationCircle, faClock);
 
 
 const App = () => {
--- src/app/Item.js
+++ src/app/Item.js
@@ -1,17 +1,55 @@
 import React from "react";
 import Accordion from "react-bootstrap/es/Accordion";
 import Card from "react-bootstrap/es/Card";
+import Row from "react-bootstrap/es/Row";
+import Col from "react-bootstrap/es/Col";
+import Image from "react-bootstrap/es/Image";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome/index.es";
 
 class Item extends React.Component {
     state = {
-        mode: "borrow", //borrow,lend
-        styleMode: "success", // borrow=success lend=danger
         color: "#28a745", // borrow = #28a745 lend = #dc3545
-        opositColor: "#dc3545",
+        state: "done", // waiting, done, overdue
+        icon: "check-circle",
+        iconColor: "green",
+        title: "Lorem ipsum",
+        date: "20.12.2019",
+        person: "John Smith",
+        shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
     };
 
     constructor(props) {
         super(props);
+        if(this.props.color !== undefined)
+            this.state.color = this.props.color;
+        if(this.props.state !== undefined)
+            this.state.state = this.props.state;
+        if(this.props.title !== undefined)
+            this.state.title = this.props.title;
+        if(this.props.date !== undefined)
+            this.state.date = this.props.date;
+        if(this.props.person !== undefined)
+            this.state.person = this.props.person;
+        if(this.props.description !== undefined)
+            this.state.description = this.props.description;
+        if(this.props.shortDescription !== undefined)
+            this.state.shortDescription = this.props.shortDescription;
+
+
+
+        if(this.state.state === "done") {
+            this.state.icon = "check-circle";
+            this.state.iconColor = "green";
+        }
+        if(this.state.state === "overdue") {
+            this.state.icon = "exclamation-circle";
+            this.state.iconColor = "red"
+        }
+        if(this.state.state === "waiting") {
+            this.state.icon = "clock";
+            this.state.iconColor = "grey"
+        }
     }
 
     render() {
@@ -21,11 +59,37 @@ class Item extends React.Component {
                 borderColor: this.state.color,
                 borderWidth: 3,
             }}>
-                <Accordion.Toggle as={Card.Header} eventKey={this.props.id}>
-                    Click me!
+                <Accordion.Toggle as={Card.Header} eventKey={this.props.id} style={{backgroundColor: 'white'}}>
+                    <Row>
+                        <Col xs={2} sm={2} md={1} lg={2} style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center'}}>
+                            <Image style={{width: '50%', minWidth: 50}} src={"https://picsum.photos/100/100/?image=" + Math.ceil(Math.random()*20)} roundedCircle />
+                        </Col>
+                        <Col xs={7} sm={7} md={4} lg={3}>
+                            <h6>{this.state.title}</h6>
+                            <Row>
+                                <Col xs={1}><FontAwesomeIcon icon="clock" color="grey" size="1x"/></Col>
+                                <Col xs={9}>{this.state.date}</Col>
+                            </Row>
+                            <Row>
+                                <Col xs={1}><FontAwesomeIcon icon="user" color="grey" size="1x"/></Col>
+                                <Col xs={9}>{this.state.person}</Col>
+                            </Row>
+                        </Col>
+                        <Col xs={1} sm={1} md={5} lg={6}>
+                            <div className="d-none d-md-block">
+                                {this.state.shortDescription}
+                            </div>
+                        </Col>
+                        <Col xs={1} sm={1} md={1} lg={1}>
+                            <FontAwesomeIcon icon="check-circle" color="green" size="2x" style={{marginTop: 15}}/>
+                        </Col>
+                    </Row>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey={this.props.id}>
-                    <Card.Body>Hello! I'm another body</Card.Body>
+                    <Card.Body>{this.state.description}</Card.Body>
                 </Accordion.Collapse>
             </Card>
 
